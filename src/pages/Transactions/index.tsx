95e47f0f7d69853f8ba24f92b51d059bfd9f88d4
markus90souza <markus90souza@gmail.com>
2022-08-09 20:47:15 -0300

    Feat:page - create table summary fro transactions page

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,12 +1,43 @@
 import { Header } from '../../components/Header'
 import { Summary } from '../../components/Summary'
-import { Container } from './styles'
+import {
+  Container,
+  TransactionsTable,
+  TransactionsTableContainer,
+  Price,
+} from './styles'
 
 const Transactions = () => {
   return (
     <Container>
       <Header />
       <Summary />
+
+      <TransactionsTableContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td>Desenvolvimento de sites</td>
+              <td>
+                R$
+                <Price variant="income">12,000,00</Price>
+              </td>
+              <td>Venda</td>
+              <td>20/08/202o</td>
+            </tr>
+
+            <tr>
+              <td>Gasolina</td>
+              <td>
+                R$
+                <Price variant="outcome"> - 800,00</Price>
+              </td>
+              <td>Transporte</td>
+              <td>22/08/202o</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsTableContainer>
     </Container>
   )
 }
--- src/pages/Transactions/styles.ts
+++ src/pages/Transactions/styles.ts
@@ -1,3 +1,42 @@
 import styled from 'styled-components'
 
 export const Container = styled.div``
+
+export const TransactionsTableContainer = styled.div`
+  width: 100%;
+  max-width: 1120px;
+  margin: 4rem auto 0;
+  padding: 0 1.5rem;
+`
+
+export const TransactionsTable = styled.table`
+  width: 100%;
+  border-collapse: separate;
+  border-spacing: 0 0.5rem;
+
+  td {
+    background-color: ${({ theme }) => theme['gray-700']};
+    padding: 1.25rem 2rem;
+
+    &:first-child {
+      width: 50%;
+      border-top-left-radius: 6px;
+      border-bottom-left-radius: 6px;
+    }
+
+    &:last-child {
+      border-top-right-radius: 6px;
+      border-bottom-right-radius: 6px;
+    }
+  }
+`
+
+type PriceProps = {
+  variant: 'income' | 'outcome'
+}
+
+export const Price = styled.span<PriceProps>`
+  margin-left: 0.5rem;
+  color: ${({ theme, variant }) =>
+    variant === 'income' ? theme['green-300'] : theme['red-300']};
+`
